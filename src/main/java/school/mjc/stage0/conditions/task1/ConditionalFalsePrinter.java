3cbccce285d5b085c0af723912e56c525c32224a
Snorvor <bulezar@gmail.com>
2025-07-26 17:09:50 -0300

    program that will print 'false'

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        if (false) {
+            System.out.println("nothing");
+        } else {
+            System.out.println("false");
+        }
     }
 }
